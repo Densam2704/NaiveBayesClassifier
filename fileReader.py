99f4676e21e45e94e6a1413a0d810e950574b303
Densam2704 <den.samoshckin@yandex.ru>
2021-05-13 21:56:49 +0300

    Added file fileReader.py

---
+++ fileReader.py
@@ -0,0 +1,25 @@
+
+def mean(list):
+    sum_num = 0
+    for t in list:
+        sum_num = sum_num + t
+
+    avg = sum_num / len(list)
+    return avg
+
+path = 'C:\Study\Programming\Python Projects\data\All sessions\\session duration.txt'
+f = open(path)
+durList=[]
+for line in f:
+    durList.append(float(line))
+    # print(durList.__len__())
+maxDur=max(durList)
+minDur=min(durList)
+meanDur=mean(durList)
+
+print("maxDur= ",maxDur)
+print("minDur= ",minDur)
+print("meanDur= ",meanDur)
+
+
+
