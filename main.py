e183ca42e33121420ee794b53ec9483e8ed67ca0
Densam2704 <den.samoshckin@yandex.ru>
2021-05-18 17:17:43 +0300

    Made functions in PlotMaker

--- main.py
+++ main.py
@@ -2,9 +2,39 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 
-X, y = load_iris(return_X_y=True)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=0)
-gnb = GaussianNB()
-y_pred = gnb.fit(X_train, y_train).predict(X_test)
-print("Number of mislabeled points out of a total %d points : %d"
-      % (X_test.shape[1], (y_test != y_pred).sum()))
+# X, y = load_iris(return_X_y=True)
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=0)
+# gnb = GaussianNB()
+# y_pred = gnb.fit(X_train, y_train).predict(X_test)
+# print("Number of mislabeled points out of a total %d points : %d"
+#       % (X_test.shape[1], (y_test != y_pred).sum()))
+from fileReader import FileReader
+from plotMaker import PlotMaker
+
+allDurPath = 'C:\Study\Programming\Python Projects\data\All sessions\\session duration.txt'
+allIntervalPath = 'C:\Study\Programming\Python Projects\data\All sessions\\packet intervals in sessions.txt'
+allLenPath = 'C:\Study\Programming\Python Projects\data\All sessions\\packet lengths in sessions.txt'
+allPathToSave = 'C:\Study\Programming\Python Projects\img\All sessions\\'
+telegramDurPath = 'C:\Study\Programming\Python Projects\data\Telegram\\telegram session duration.txt'
+telegramIntervalPath = 'C:\Study\Programming\Python Projects\data\Telegram\\telegram packet intervals in sessions.txt'
+telegramLenPath = 'C:\Study\Programming\Python Projects\data\Telegram\\telegram packet lengths in sessions.txt'
+telegramPathToSave = 'C:\Study\Programming\Python Projects\img\Telegram\\'
+discordDurPath = 'C:\Study\Programming\Python Projects\data\Discord\\discord session duration.txt'
+discordIntervalPath = 'C:\Study\Programming\Python Projects\data\Discord\\discord packet intervals in sessions.txt'
+discordLenPath = 'C:\Study\Programming\Python Projects\data\Discord\\discord packet lengths in sessions.txt'
+discordPathToSave = 'C:\Study\Programming\Python Projects\img\Discord\\'
+
+allReader = FileReader(allDurPath, allIntervalPath, allLenPath)
+telegramReader = FileReader(telegramDurPath, telegramIntervalPath, telegramLenPath)
+discordReader = FileReader(discordDurPath, discordIntervalPath, discordLenPath)
+
+allPlotMaker = PlotMaker(allReader, allPathToSave)
+allPlotMaker.make()
+
+telegramPlotMaker = PlotMaker(telegramReader, telegramPathToSave)
+telegramPlotMaker.make()
+
+discordPlotMaker = PlotMaker(discordReader,discordPathToSave)
+discordPlotMaker.make()
+
+PlotMaker.showPlots()
--- plotMaker.py
+++ plotMaker.py
@@ -1,61 +1,91 @@
 import matplotlib.pyplot as plt
 from fileReader import FileReader
 
-durPath = 'C:\Study\Programming\Python Projects\data\All sessions\\session duration.txt'
-intervalPath = 'C:\Study\Programming\Python Projects\data\All sessions\\packet intervals in sessions.txt'
-lenPath = 'C:\Study\Programming\Python Projects\data\All sessions\\packet lengths in sessions.txt'
-pathToSave = 'C:\Study\Programming\Python Projects\img\All sessions\\'
-fileReader = FileReader(durPath, intervalPath, lenPath)
+class FigureNum:
+    figureNum = 1
 
-durations = fileReader.getDurations()
-lengths = fileReader.getLengths()
-intervals = fileReader.getIntervals()
-lengthPerSessionList = fileReader.getLengthsPerSession()
-intensities = fileReader.getIntensities()
+class PlotMaker:
+    fileReader = ""
+    pathToSave = ""
 
-plot1 = plt.figure(1,figsize=(8,6))
-plt.subplot(211)
-plt.title("Session Lengths", fontsize=14, fontname='Times New Roman')
-plt.ylabel("Length, bytes", fontsize=12, fontname='Times New Roman')
-plt.scatter(durations, lengthPerSessionList)
-plt.subplot(212)
-plt.tight_layout(w_pad=0.5,h_pad=2.5)
-plt.title("Session Length log scale", fontsize=14, fontname='Times New Roman')
-plt.ylabel("Length, bytes", fontsize=12, fontname='Times New Roman',labelpad=0.5)
-plt.xlabel("Duration, s", fontsize=12, fontname='Times New Roman',labelpad=1.1)
-plt.yscale('log')
-plt.scatter(durations, lengthPerSessionList)
-plt.savefig(pathToSave + 'Length(duration).png')
+    def __init__(self, fileReader, pathToSave):
+        self.fileReader = fileReader
+        self.pathToSave = pathToSave
 
-plot2 = plt.figure(2,figsize=(8, 6))
-plt.subplot(211)
-plt.title("Session Intensity", fontsize=14, fontname='Times New Roman')
-plt.ylabel("Intensity,\n bytes per second", fontsize=14, fontname='Times New Roman')
-plt.scatter(durations, intensities)
-plt.subplot(212)
-plt.tight_layout(w_pad=2,h_pad=2)
-plt.title("Session Intensity log scale", fontsize=14, fontname='Times New Roman')
-plt.ylabel("Intensity,\n bytes per second", fontsize=14, fontname='Times New Roman')
-plt.xlabel("Duration, s", fontsize=14, fontname='Times New Roman')
-plt.yscale('log')
-plt.scatter(durations, lengthPerSessionList)
-plt.savefig(pathToSave + 'Intensity(duration).png')
+    def make(self):
+        self.makePlot1()
+        self.makePlot2()
+        self.makePlot3()
 
-# figsize has default float params 6 * 4 inches
-plot3 = plt.figure(3, figsize=(8, 4))
-plt.subplot(121)
-plt.title("Intensity(Length)", fontsize=14, fontname='Times New Roman')
-plt.xlabel("Length, bytes", fontsize=14, fontname='Times New Roman')
-plt.ylabel("Intensity,\n bytes per second", fontsize=14, fontname='Times New Roman')
-plt.scatter(lengthPerSessionList, intensities)
-plt.subplot(122)
-plt.title("Intensity(Length) log scale", fontsize=14, fontname='Times New Roman')
-plt.xlabel("Length, bytes", fontsize=14, fontname='Times New Roman')
-plt.xscale('log')
-plt.scatter(lengthPerSessionList, intensities)
-plt.savefig(pathToSave + 'Intensity(lengths).png')
-plt.tight_layout(h_pad=-0.88)
+    def makePlot1(self):
+        durations = self.fileReader.getDurations()
+        lengthPerSessionList = self.fileReader.getLengthsPerSession()
+        pathToSave = self.pathToSave
+        plot1 = plt.figure(FigureNum.figureNum, figsize=(8, 6))
+        plt.subplot(211)
+        plt.title("Session Lengths", fontsize=14, fontname='Times New Roman')
+        plt.ylabel("Length, bytes", fontsize=12, fontname='Times New Roman')
+        plt.scatter(durations, lengthPerSessionList)
+        plt.subplot(212)
+        plt.title("Session Length log scale", fontsize=14, fontname='Times New Roman')
+        plt.ylabel("Length, bytes", fontsize=12, fontname='Times New Roman')
+        plt.xlabel("Duration, s", fontsize=12, fontname='Times New Roman')
+        plt.yscale('log')
+        plt.scatter(durations, lengthPerSessionList)
+        plt.savefig(pathToSave + 'Length(duration).png')
+        plt.tight_layout(h_pad=1)
 
-plt.show()
+        self.nextFigure()
 
-input()
+    def makePlot2(self):
+        durations = self.fileReader.getDurations()
+        intensities = self.fileReader.getIntensities()
+        pathToSave = self.pathToSave
+
+        plot2 = plt.figure(FigureNum.figureNum, figsize=(8, 6))
+        plt.subplot(211)
+        plt.title("Session Intensity", fontsize=14, fontname='Times New Roman')
+        plt.ylabel("Intensity,\n bytes per second", fontsize=14, fontname='Times New Roman')
+        plt.scatter(durations, intensities, co)
+        plt.subplot(212)
+        plt.title("Session Intensity log scale", fontsize=14, fontname='Times New Roman')
+        plt.ylabel("Intensity,\n bytes per second", fontsize=14, fontname='Times New Roman')
+        plt.xlabel("Duration, s", fontsize=14, fontname='Times New Roman', labelpad=0.1)
+        plt.yscale('log')
+        plt.scatter(durations, intensities)
+        plt.savefig(pathToSave + 'Intensity(duration).png')
+        plt.tight_layout(h_pad=1)
+
+        self.nextFigure()
+
+    def makePlot3(self):
+        lengthPerSessionList = self.fileReader.getLengthsPerSession()
+        intensities = self.fileReader.getIntensities()
+        pathToSave = self.pathToSave
+
+        plot3 = plt.figure(FigureNum.figureNum, figsize=(8, 4))
+        plt.subplot(121)
+        plt.title("Intensity(Length)", fontsize=14, fontname='Times New Roman')
+        plt.xlabel("Length, bytes", fontsize=14, fontname='Times New Roman')
+        plt.ylabel("Intensity,\n bytes per second", fontsize=14, fontname='Times New Roman')
+        plt.xscale('log')
+        plt.scatter(lengthPerSessionList, intensities)
+        plt.subplot(122)
+        plt.title("Intensity(Length) log scale", fontsize=14, fontname='Times New Roman')
+        plt.xlabel("Length, bytes", fontsize=14, fontname='Times New Roman')
+
+        plt.scatter(lengthPerSessionList, intensities)
+        plt.savefig(pathToSave + 'Intensity(lengths).png')
+        plt.tight_layout(h_pad=-0.88)
+
+        self.nextFigure()
+
+    @staticmethod
+    def showPlots(self):
+            plt.show()
+
+    def nextFigure(self):
+        FigureNum.figureNum+=1
+        print(FigureNum.figureNum)
+
+# input()
